8b91ea3d5fccdd5ac7a529355be4a38279805299
fk-ajain <ankurjain.v@flipkart.com>
2017-11-02 17:33:47 +0530

    Instrumentation of already loaded system classes with fileinputstream as example

--- bciagent/src/main/java/fk/prof/bciagent/BciAgent.java
+++ bciagent/src/main/java/fk/prof/bciagent/BciAgent.java
@@ -1,13 +1,39 @@
 package fk.prof.bciagent;
 
-import fk.prof.InstrumentationStub;
 
+import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BciAgent {
   public static void premain(String agentArgs, Instrumentation inst) {
     System.out.println("Starting the agent");
-    inst.addTransformer(new ProfileMethodTransformer());
+    Class[] classes = inst.getAllLoadedClasses();
+    List<Class> modifiableClasses = new ArrayList<>();
+    System.out.println("Already loaded classes\n--------------");
+    for(int i = 0; i< classes.length;i++) {
+      boolean isModifiable = inst.isModifiableClass(classes[i]);
+      System.out.println(isModifiable + " " + classes[i].getName());
+      if(isModifiable) {
+        modifiableClasses.add(classes[i]);
+      }
+    }
+    System.out.println("----------------------");
+
+    ClassFileTransformer transformer = new ProfileMethodTransformer();
+    inst.addTransformer(transformer, true);
+
+    Class[] retransformClasses = new Class[modifiableClasses.size()];
+    for(int j = 0;j<modifiableClasses.size();j++) {
+      retransformClasses[j] = modifiableClasses.get(j);
+    }
+    try {
+      inst.retransformClasses(retransformClasses);
+    } catch (Exception ex) {
+      System.out.println("Error in retransforming: " + ex.getMessage());
+      ex.printStackTrace();
+    }
   }
 }
--- bciagent/src/test/java/fk/prof/bciagent/ProfileType.java
+++
@@ -1,5 +0,0 @@
-package fk.prof.bciagent;
-
-public enum ProfileType {
-  OFF_CPU
-}
